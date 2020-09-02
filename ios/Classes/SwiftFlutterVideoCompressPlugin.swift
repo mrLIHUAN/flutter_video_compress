38878d9637142f69c24929987620cce290599950
lihuan <1139116598@qq.com>
2020-09-02 11:34:17 +0800

    【改回修改】

--- ios/Classes/SwiftFlutterVideoCompressPlugin.swift
+++ ios/Classes/SwiftFlutterVideoCompressPlugin.swift
@@ -182,113 +182,109 @@ public class SwiftFlutterVideoCompressPlugin: NSObject, FlutterPlugin {
         let nameSuffix = isIncludeAudio ? "" : "-preview"
         let compressionUrl =
             Utility.getPathUrl("\(Utility.basePath())/\(Utility.getFileName(path))\(nameSuffix).\(sourceVideoType)")
-        let encoder = SDAVAssetExportSession(asset: sourceVideoAsset);
-        encoder?.outputFileType = AVFileType.mp4.rawValue;
-        encoder?.outputURL = compressionUrl;
-        encoder?.videoSettings = [
-            AVVideoCodecKey: AVVideoCodecH264,
-            AVVideoWidthKey: 720,
-            AVVideoHeightKey: 1280,
-            AVVideoCompressionPropertiesKey:
-            [
-                AVVideoAverageBitRateKey: 3000000,
-                AVVideoProfileLevelKey: AVVideoProfileLevelH264High40,
-            ],
-        ];
-        encoder?.audioSettings = [
-            AVFormatIDKey: kAudioFormatMPEG4AAC,
-            AVNumberOfChannelsKey: 2,
-            AVSampleRateKey: 44100,
-            AVEncoderBitRateKey: 128000,
-        ]
+//        let encoder = SDAVAssetExportSession(asset: sourceVideoAsset);
+//        encoder?.outputFileType = AVFileType.mp4.rawValue;
+//        encoder?.outputURL = compressionUrl;
+//        encoder?.videoSettings = [
+//            AVVideoCodecKey: AVVideoCodecH264,
+//            AVVideoWidthKey: 720,
+//            AVVideoHeightKey: 1280,
+//            AVVideoCompressionPropertiesKey:
+//            [
+//                AVVideoAverageBitRateKey: 3000000,
+//                AVVideoProfileLevelKey: AVVideoProfileLevelH264High40,
+//            ],
+//        ];
+//        encoder?.audioSettings = [
+//            AVFormatIDKey: kAudioFormatMPEG4AAC,
+//            AVNumberOfChannelsKey: 2,
+//            AVSampleRateKey: 44100,
+//            AVEncoderBitRateKey: 128000,
+//        ]
+//
+//        encoder?.exportAsynchronously(completionHandler: {
+//            if (encoder!.status == AVAssetExportSession.Status.completed)
+//            {
+//
+//                var json = self.getMediaInfoJson(compressionUrl.absoluteString)
+//                  json["isCancel"] = false
+//                  let jsonString = Utility.keyValueToJson(json)
+//                  result(jsonString)
+//                print("Video export succeeded");
+//            }
+//            else if (encoder!.status == AVAssetExportSession.Status.cancelled)
+//            {
+//                print("Video export cancelled");
+//            }
+//            else
+//            {
+//                print("Video export failed with error: \(encoder?.error.localizedDescription ?? "") (\(encoder?.error.debugDescription ?? "")");
+//            }
+//        });
+//
         
-        encoder?.exportAsynchronously(completionHandler: {
-            if (encoder!.status == AVAssetExportSession.Status.completed)
-            {
-                
-                var json = self.getMediaInfoJson(compressionUrl.absoluteString)
-                  json["isCancel"] = false
-                  let jsonString = Utility.keyValueToJson(json)
-                  result(jsonString)
-                print("Video export succeeded");
-            }
-            else if (encoder!.status == AVAssetExportSession.Status.cancelled)
-            {
-                print("Video export cancelled");
-            }
-            else
-            {
-                print("Video export failed with error: \(encoder?.error.localizedDescription ?? "") (\(encoder?.error.debugDescription ?? "")");
-            }
-        });
-        
-        
-        
-        
+        let timescale = sourceVideoAsset.duration.timescale
+        let minStartTime = Double(startTime ?? 0)
 
-        
-//        let timescale = sourceVideoAsset.duration.timescale
-//        let minStartTime = Double(startTime ?? 0)
-//
-//        let videoDuration = sourceVideoAsset.duration.seconds
-//        let minDuration = Double(duration ?? videoDuration)
-//        let maxDurationTime = minStartTime + minDuration < videoDuration ? minDuration : videoDuration
-//
-//        let cmStartTime = CMTimeMakeWithSeconds(minStartTime, preferredTimescale: timescale)
-//        let cmDurationTime = CMTimeMakeWithSeconds(maxDurationTime, preferredTimescale: timescale)
-//        let timeRange: CMTimeRange = CMTimeRangeMake(start: cmStartTime, duration: cmDurationTime)
-//
-//        let session = getComposition(isIncludeAudio, timeRange, sourceVideoTrack!)
-//
-//        let exporter = AVAssetExportSession(asset: session, presetName: getExportPreset(quality))!
-//
-//        exporter.outputURL = compressionUrl
-//        exporter.outputFileType = AVFileType.mp4
-//        exporter.shouldOptimizeForNetworkUse = true
-//
-//        if frameRate != nil {
-//            let videoComposition = AVMutableVideoComposition(propertiesOf: sourceVideoAsset)
-//            videoComposition.frameDuration = CMTimeMake(value: 1, timescale: Int32(frameRate!))
-//            exporter.videoComposition = videoComposition
-//        }
-//
-//        if !isIncludeAudio {
-//            exporter.timeRange = timeRange
-//        }
-//
-//        Utility.deleteFile(compressionUrl.absoluteString)
-//
-//        let timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.updateProgress),
-//                                         userInfo: exporter, repeats: true)
-//
-//        exporter.exportAsynchronously(completionHandler: {
-//            if(self.stopCommand) {
-//                timer.invalidate()
-//                self.stopCommand = false
-//                var json = self.getMediaInfoJson(path)
-//                json["isCancel"] = true
-//                let jsonString = Utility.keyValueToJson(json)
-//                return result(jsonString)
-//            }
-//            if deleteOrigin {
-//                timer.invalidate()
-//                let fileManager = FileManager.default
-//                do {
-//                    if fileManager.fileExists(atPath: path) {
-//                        try fileManager.removeItem(atPath: path)
-//                    }
-//                    self.exporter = nil
-//                    self.stopCommand = false
-//                }
-//                catch let error as NSError {
-//                    print(error)
-//                }
-//            }
-//            var json = self.getMediaInfoJson(compressionUrl.absoluteString)
-//            json["isCancel"] = false
-//            let jsonString = Utility.keyValueToJson(json)
-//            result(jsonString)
-//        })
+        let videoDuration = sourceVideoAsset.duration.seconds
+        let minDuration = Double(duration ?? videoDuration)
+        let maxDurationTime = minStartTime + minDuration < videoDuration ? minDuration : videoDuration
+
+        let cmStartTime = CMTimeMakeWithSeconds(minStartTime, preferredTimescale: timescale)
+        let cmDurationTime = CMTimeMakeWithSeconds(maxDurationTime, preferredTimescale: timescale)
+        let timeRange: CMTimeRange = CMTimeRangeMake(start: cmStartTime, duration: cmDurationTime)
+
+        let session = getComposition(isIncludeAudio, timeRange, sourceVideoTrack!)
+
+        let exporter = AVAssetExportSession(asset: session, presetName: getExportPreset(quality))!
+
+        exporter.outputURL = compressionUrl
+        exporter.outputFileType = AVFileType.mp4
+        exporter.shouldOptimizeForNetworkUse = true
+
+        if frameRate != nil {
+            let videoComposition = AVMutableVideoComposition(propertiesOf: sourceVideoAsset)
+            videoComposition.frameDuration = CMTimeMake(value: 1, timescale: Int32(frameRate!))
+            exporter.videoComposition = videoComposition
+        }
+
+        if !isIncludeAudio {
+            exporter.timeRange = timeRange
+        }
+
+        Utility.deleteFile(compressionUrl.absoluteString)
+
+        let timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.updateProgress),
+                                         userInfo: exporter, repeats: true)
+
+        exporter.exportAsynchronously(completionHandler: {
+            if(self.stopCommand) {
+                timer.invalidate()
+                self.stopCommand = false
+                var json = self.getMediaInfoJson(path)
+                json["isCancel"] = true
+                let jsonString = Utility.keyValueToJson(json)
+                return result(jsonString)
+            }
+            if deleteOrigin {
+                timer.invalidate()
+                let fileManager = FileManager.default
+                do {
+                    if fileManager.fileExists(atPath: path) {
+                        try fileManager.removeItem(atPath: path)
+                    }
+                    self.exporter = nil
+                    self.stopCommand = false
+                }
+                catch let error as NSError {
+                    print(error)
+                }
+            }
+            var json = self.getMediaInfoJson(compressionUrl.absoluteString)
+            json["isCancel"] = false
+            let jsonString = Utility.keyValueToJson(json)
+            result(jsonString)
+        })
     }
     
     private func cancelCompression(_ result: FlutterResult) {
